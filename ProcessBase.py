95d404197eeb11b09398d6da24b5f02a711e6655
mdjibran <mohdjibran316@gmail.com>
2017-02-08 14:57:20 -0500

    Updated display method

--- ProcessBase.py
+++ ProcessBase.py
@@ -1,3 +1,5 @@
+import math
+
 class ProcessBase:
 
     def __init__(self, descString, resumeString):
@@ -39,21 +41,31 @@ class ProcessBase:
                     else:
                         self.resumeKeyValues[word] = 1
 
+    def formatString(self, k_desc, v_desc, v_resume):
+        k_descLen = len(k_desc)
+        spcLen = 30 - k_descLen
+        spcLen = int(math.ceil(spcLen/2))
+        displyStr = "|"+" "*spcLen + k_desc + spcLen*" " + "|   " + str(v_desc) + "   |   " + str(v_resume) + "   |   "
+        return displyStr
+
     def compareKeywords(self):
         self.getDescKeywords()
         self.getResumeKeywords()
         resumePer = 0
         totresumePer = 0
         totdescPer = 0
-
+        print "________________________________________________"
+        print "|               KEY            |  DESC | VALUE |"
+        print "------------------------------------------------"
         for k_desc, v_desc in self.descKeyValues.items():
             totdescPer += 100
             if k_desc in self.resumeKeyValues:
                 v_resume = str(self.resumeKeyValues[k_desc])
                 resumePer = float(float(v_resume) / float(v_desc) * 100)
-                print "Key : " + str(k_desc) + " |Desc : " + str(v_desc) + " |Resume : " + v_resume
+                print self.formatString(k_desc, v_desc, v_resume)
                 totresumePer += resumePer
             else:
-                print "Key : " + str(k_desc) + " |Desc : " + str(v_desc) + " |Resume : 0"
-
-        print "\n\n Job :" + str(totdescPer) + " | Resume : " + str(totresumePer)
+                print self.formatString(k_desc, v_desc, 0)
+        print "-------------------------------------------------"
+        #print "\n\n Job :" + str(totdescPer) + " | Resume : " + str(totresumePer)
+        print "Profile Match : " + str(int(math.ceil((totresumePer/totdescPer)*100)))
